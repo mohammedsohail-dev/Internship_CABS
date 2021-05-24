80e31df5cdfde81b67d8c8b1e1d74eb92b3933af
Mahalti Mohammed Sohail <75085444+mohammedsohail-dev@users.noreply.github.com>
2021-05-24 17:28:07 +0530

    Add files via upload

---
+++ LSTM.py
@@ -0,0 +1,302 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, LSTM, BatchNormalization
+from tensorflow.keras.callbacks import TensorBoard, ModelCheckpoint
+import tensorflow.keras.backend as K
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+import os
+from sklearn import preprocessing
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Bidirectional,GRU
+from tensorflow.keras.optimizers import Adam, Adagrad, RMSprop
+from sklearn import metrics
+from itertools import permutations
+import sklearn
+np.random.seed(1234)  
+PYTHONHASHSEED = 0
+
+model_path = 'C:/Users/msoha/OneDrive/Documents/Mywork/models'
+
+
+def mean_prediction_score(RUL_real, RUL_pred):
+    d = RUL_pred - RUL_real
+    
+    return (np.sum(np.exp(d[d >= 0] / 13) - 1) +
+            np.sum(np.exp(-1 * d[d < 0] / 10) - 1)) 
+
+
+train_df = pd.read_csv('C:/Users/msoha/OneDrive/Documents/DATASET/RUL/train_FD004.txt', sep=" ", header=None)
+train_df.drop(train_df.columns[[26, 27]], axis=1, inplace=True)
+train_df.columns = ['id', 'cycle', 'setting1', 'setting2', 'setting3', 's1', 's2', 's3',
+                     's4', 's5', 's6', 's7', 's8', 's9', 's10', 's11', 's12', 's13', 's14',
+                     's15', 's16', 's17', 's18', 's19', 's20', 's21']
+
+train_df = train_df.sort_values(['id','cycle'])
+
+
+test_df = pd.read_csv('C:/Users/msoha/OneDrive/Documents/DATASET/RUL/test_FD004.txt', sep=" ", header=None)
+test_df.drop(test_df.columns[[26, 27]], axis=1, inplace=True)
+test_df.columns = ['id', 'cycle', 'setting1', 'setting2', 'setting3', 's1', 's2', 's3',
+                     's4', 's5', 's6', 's7', 's8', 's9', 's10', 's11', 's12', 's13', 's14',
+                     's15', 's16', 's17', 's18', 's19', 's20', 's21']
+
+truth_df = pd.read_csv('C:/Users/msoha/OneDrive/Documents/DATASET/RUL/RUL_FD004.txt', sep=" ", header=None)
+truth_df.drop(truth_df.columns[[1]], axis=1, inplace=True)
+
+
+rul = pd.DataFrame(train_df.groupby('id')['cycle'].max()).reset_index()
+rul.columns = ['id', 'max']
+train_df = train_df.merge(rul, on=['id'], how='left')
+train_df['RUL'] = train_df['max'] - train_df['cycle']
+train_df.drop('max', axis=1, inplace=True)
+
+
+
+
+
+train_df['cycle_norm'] = train_df['cycle']
+cols_normalize = train_df.columns.difference(['id','cycle','RUL'])
+min_max_scaler = preprocessing.MinMaxScaler()
+norm_train_df = pd.DataFrame(min_max_scaler.fit_transform(train_df[cols_normalize]), 
+                             columns=cols_normalize, 
+                             index=train_df.index)
+join_df = train_df[train_df.columns.difference(cols_normalize)].join(norm_train_df)
+train_df = join_df.reindex(columns = train_df.columns)
+
+
+
+test_df['cycle_norm'] = test_df['cycle']
+norm_test_df = pd.DataFrame(min_max_scaler.transform(test_df[cols_normalize]), 
+                            columns=cols_normalize, 
+                            index=test_df.index)
+test_join_df = test_df[test_df.columns.difference(cols_normalize)].join(norm_test_df)
+test_df = test_join_df.reindex(columns = test_df.columns)
+test_df = test_df.reset_index(drop=True)
+print(test_df.head())
+
+
+rul = pd.DataFrame(test_df.groupby('id')['cycle'].max()).reset_index()
+rul.columns = ['id', 'max']
+truth_df.columns = ['more']
+truth_df['id'] = truth_df.index + 1
+truth_df['max'] = rul['max'] + truth_df['more']
+truth_df.drop('more', axis=1, inplace=True)
+
+
+test_df = test_df.merge(truth_df, on=['id'], how='left')
+test_df['RUL'] = test_df['max'] - test_df['cycle']
+test_df.drop('max', axis=1, inplace=True)
+
+
+def multi_correl(a1,b1,c1):
+    
+     ac=a1.corr(c1)
+     bc=b1.corr(c1)
+     ab=a1.corr(b1)
+    
+     res=(((ac**2)+(bc**2)-2*ac*bc*ab)/(1-ab**2))**0.5
+     return(res)
+
+
+sequence_length = 50
+
+
+def gen_sequence(id_df, seq_length, seq_cols):
+
+    data_matrix = id_df[seq_cols].values
+    num_elements = data_matrix.shape[0]
+  
+    for start, stop in zip(range(0, num_elements-seq_length), range(seq_length, num_elements)):
+        yield data_matrix[start:stop, :]
+        
+
+sensor_cols = ['s' + str(i) for i in range(1,22)]
+sequence_cols = ['setting1', 'setting2', 'setting3', 'cycle_norm']
+sequence_cols.extend(sensor_cols)
+
+
+val=list(gen_sequence(train_df[train_df['id']==1], sequence_length, sequence_cols))
+print(len(val))
+
+
+seq_gen = (list(gen_sequence(train_df[train_df['id']==id], sequence_length, sequence_cols)) 
+           for id in train_df['id'].unique())
+
+
+seq_array = np.concatenate(list(seq_gen)).astype(np.float32)
+print(seq_array.shape)
+
+
+def gen_labels(id_df, seq_length, label):
+
+    data_matrix = id_df[label].values
+    num_elements = data_matrix.shape[0]
+
+    return data_matrix[seq_length:num_elements, :]
+
+# generate labels
+label_gen = [gen_labels(train_df[train_df['id']==id], sequence_length, ['RUL']) 
+             for id in train_df['id'].unique()]
+
+label_array = np.concatenate(label_gen).astype(np.float32)
+label_array.shape
+
+
+
+def r2_keras(y_true, y_pred):
+    """Coefficient of Determination 
+    """
+    SS_res =  K.sum(K.square( y_true - y_pred ))
+    SS_tot = K.sum(K.square( y_true - K.mean(y_true) ) )
+    return ( 1 - SS_res/(SS_tot + K.epsilon()) )
+
+
+nb_features = seq_array.shape[2]
+nb_out = label_array.shape[1]
+
+model = Sequential()
+model.add((LSTM(
+         units=100,
+         return_sequences=True,input_shape=(sequence_length, nb_features))))
+model.add(Dropout(0.5))
+model.add(LSTM(
+          units=50,
+          return_sequences=False))
+model.add(Dropout(0.5))
+model.add(Dense(units=nb_out,activation="linear"))
+model.compile(loss='mean_squared_error', optimizer='RMSprop' ,metrics=['mae',r2_keras])
+
+
+print(model.summary())
+
+
+history = model.fit(seq_array, label_array, epochs=55, batch_size=200, validation_split=0.05, verbose=2,
+          callbacks = [tf.keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0, patience=10, verbose=0, mode='min'),
+                       tf.keras.callbacks.ModelCheckpoint(model_path,monitor='val_loss', save_best_only=True, mode='min', verbose=0)]
+          )
+
+
+print(history.history.keys())
+
+
+fig_acc = plt.figure(figsize=(10, 10))
+plt.plot(history.history['r2_keras'])
+plt.plot(history.history['val_r2_keras'])
+plt.title('model r^2')
+plt.ylabel('R^2')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+fig_acc.savefig("C:/Users/msoha/model_r2.png")
+
+
+fig_acc = plt.figure(figsize=(10, 10))
+plt.plot(history.history['mae'])
+plt.plot(history.history['val_mae'])
+plt.title('model MAE')
+plt.ylabel('MAE')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+fig_acc.savefig("C:/Users/msoha/model_mae.png")
+
+
+fig_acc = plt.figure(figsize=(10, 10))
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+fig_acc.savefig("C:/Users/msoha/model_regression_loss.png")
+
+
+scores = model.evaluate(seq_array, label_array, verbose=1, batch_size=200)
+print('\nMAE: {}'.format(scores[1]))
+print('\nR^2: {}'.format(scores[2]))
+
+y_pred = model.predict(seq_array,verbose=1, batch_size=200)
+y_true = label_array
+
+test_set = pd.DataFrame(y_pred)
+test_set.to_csv('C:/Users/msoha/submit_train.csv', index = None)
+
+
+seq_array_test_last = [test_df[test_df['id']==id][sequence_cols].values[-sequence_length:] 
+                       for id in test_df['id'].unique() if len(test_df[test_df['id']==id]) >= sequence_length]
+
+seq_array_test_last = np.asarray(seq_array_test_last).astype(np.float32)
+print("seq_array_test_last")
+#print(seq_array_test_last)
+print(seq_array_test_last.shape)
+
+
+y_mask = [len(test_df[test_df['id']==id]) >= sequence_length for id in test_df['id'].unique()]
+label_array_test_last = test_df.groupby('id')['RUL'].nth(-1)[y_mask].values
+label_array_test_last = label_array_test_last.reshape(label_array_test_last.shape[0],1).astype(np.float32)
+print(label_array_test_last.shape)
+print("label_array_test_last")
+print(label_array_test_last)
+
+
+scores_test = model.evaluate(seq_array_test_last, label_array_test_last, verbose=2)
+print('\nMAE: {}'.format(scores_test[1]))
+print('\nR^2: {}'.format(scores_test[2]))
+
+y_pred_test = model.predict(seq_array_test_last)
+y_true_test = label_array_test_last
+
+test_set = pd.DataFrame(y_pred_test)
+test_set.to_csv('C:/Users/msoha/submit_test.csv', index = None)
+
+
+fig_verify = plt.figure(figsize=(100, 50))
+plt.plot(y_pred_test, color="blue")
+plt.plot(y_true_test, color="green")
+plt.title('prediction')
+plt.ylabel('value')
+plt.xlabel('row')
+plt.legend(['predicted', 'actual data'], loc='upper left')
+plt.show()
+fig_verify.savefig("C:/Users/msoha/model_regression_verify.png")
+
+
+predicted_data_last=[]
+for i in range(len(seq_array_test_last)):
+    for j in range(sequence_length):
+        predicted_data_last.append(y_pred_test[i])
+        
+test_input_data=seq_array_test_last.reshape(11400,25)
+overallcomp=np.hstack((test_input_data,predicted_data_last))
+
+overallcompp=pd.DataFrame(overallcomp)
+corrr=[]
+score1=[]
+
+
+for i in range(25):
+  corrr.append(overallcompp[i].corr(overallcompp[25]))
+  score1.append(sklearn.metrics.mutual_info_score(overallcompp[i],overallcompp[25]))
+
+max1=corrr.index(max(corrr))
+
+percentscore=score1/sum(score1)
+print(percentscore)
+
+print(max1)
+combo2=[]
+x3=[]
+perm=permutations([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24],2)
+for i in list(perm):
+    x3.append(i)
+    
+for i in range(len(x3)):
+    ab1=x3[i][0]
+    bc1=x3[i][1]
+    combo2.append((multi_correl(overallcompp[ab1],overallcompp[bc1],overallcompp[25])))
+
+maxcomb2=x3[combo2.index(max(combo2))]
+
+print(maxcomb2)
